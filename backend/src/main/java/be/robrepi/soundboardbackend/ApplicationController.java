bf4e53115bbc9edbe054ecd9de6e40ac18c7fe9f
Pieter-Jan Robrecht <robrecht_pieterjan@hotmail.com>
2023-07-02 21:12:51 +0200

    Websocket example

--- backend/src/main/java/be/robrepi/soundboardbackend/ApplicationController.java
+++ backend/src/main/java/be/robrepi/soundboardbackend/ApplicationController.java
@@ -1,13 +1,17 @@
 package be.robrepi.soundboardbackend;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.util.HtmlUtils;
 
 @Controller
 public class ApplicationController {
 
-    @RequestMapping("/")
-    public String getHomePage(){
-        return "index";
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
     }
 }
